41e233d8f08059921c76f23eae100ab188523939
Timoteo Ponce <timo.slack@gmail.com>
2019-08-05 10:05:07 -0400

    Initial commit

---
+++ src/main/java/org/timo/logviewer/LogviewerApplication.java
@@ -0,0 +1,13 @@
+package org.timo.logviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogviewerApplication.class, args);
+	}
+
+}
